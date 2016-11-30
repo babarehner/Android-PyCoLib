3f9f8c31c9e57d6e7de45069eedf1f067620ed48
Mike <babarehner@google.com>
2016-11-30 12:13:29 -0500

    Add layout items to activity_edit Create menu file menu_edit Instantiate menu in EditActivity

--- app/src/main/java/com/babarehner/android/pycolib/EditActivity.java
+++ app/src/main/java/com/babarehner/android/pycolib/EditActivity.java
@@ -2,6 +2,8 @@ package com.babarehner.android.pycolib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+    }
+
+    // Options menu automatially called from onCreate I believe
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    // Select from the options menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                // save book in db
+                return true;
+            case R.id.action_delete_all_data:
+                // delete book in db
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
