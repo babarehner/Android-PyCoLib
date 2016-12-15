96fa5ab6ff8b9a0efbe7bf65c76db27a4deaf88b
Mike Rehner <babarehner@google.com>
2016-12-15 09:49:16 -0500

    File(s): BookACtivity Implement CursorLoader to allow DB operations to run on a separate thread.

--- app/src/main/java/com/babarehner/android/pycolib/BookActivity.java
+++ app/src/main/java/com/babarehner/android/pycolib/BookActivity.java
@@ -1,7 +1,11 @@
 package com.babarehner.android.pycolib;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,31 +21,45 @@ import android.widget.Toast;
 
 import com.babarehner.android.pycolib.data.LibraryContract;
 
-public class BookActivity extends AppCompatActivity {
+public class BookActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    public static final int EXISTING_BOOK_LOADER = 0;
+
+    private Uri mCurrentLibraryUri = null;
 
     private EditText mTitleEditText;
     private EditText mAuthorEditText;
     private Spinner mPublishYearSpinner;
-    private int mPublishYear = 2016;
     private EditText mBorrowerEditText;
 
-    private Uri mCurrentLibraryUri = null;
+    private int mPublishYear;
+
+    // Uri for the current book item
+    private Uri mCurrentBookUri;
+
+    // The first year possible for the publish year
+    // used to get position in the  spinner
+    public static final int START_YEAR = 1991;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
-
+        
         //Get intent and get data from intent
         Intent intent = getIntent();
-        Uri currentLibraryUri = intent.getData();
+        mCurrentBookUri = intent.getData();
 
         // Idf the intent does not contain an single item Uri
         // FAB clicked
-        if (currentLibraryUri == null){
+        if (mCurrentBookUri == null){
+            // set page header to add book
             setTitle(getString(R.string.book_activity_title_add_book));
+            // take out the delet menu
+            invalidateOptionsMenu();
         } else {                            // individual item clicked
             setTitle(getString(R.string.book_activity_title_edit_book));
+            getLoaderManager().initLoader(EXISTING_BOOK_LOADER, null, BookActivity.this);
         }
 
         mTitleEditText = (EditText) findViewById(R.id.edit_title);
@@ -67,7 +85,7 @@ public class BookActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
                 String selection = (String) parent.getItemAtPosition(position);
                 Log.v("BookActivity ",  selection);
-                mPublishYear = Integer.parseInt(selection);
+                mPublishYear = Integer.parseInt(selection);  //The item at pos
             }
             // Because AdapterView is an abstract classe, onNothingSelected must be defined
             @Override
@@ -106,13 +124,14 @@ public class BookActivity extends AppCompatActivity {
         // read from EditText input fields
         String titleString = mTitleEditText.getText().toString().trim();
         String authorString = mAuthorEditText.getText().toString().trim();
-        String publishDateString = Integer.toString(mPublishYear);
+        //String publishYearString = String.valueOf(mPublishYear);
         String borrowerString = mBorrowerEditText.getText().toString().trim();
 
         ContentValues values = new ContentValues();
         values.put(LibraryContract.LibraryEntry.COL_TITLE, titleString);
         values.put(LibraryContract.LibraryEntry.COL_AUTHOR, authorString);
-        values.put(LibraryContract.LibraryEntry.COL_YEAR_PUBLISHED, publishDateString);
+        values.put(LibraryContract.LibraryEntry.COL_YEAR_PUBLISHED, mPublishYear);
+        Log.v("BookActivity"," mPublishYear " + mPublishYear);
         values.put(LibraryContract.LibraryEntry.COL_BORROWER, borrowerString);
 
         Uri newUri = getContentResolver().insert(LibraryContract.LibraryEntry.CONTENT_URI, values);
@@ -123,15 +142,56 @@ public class BookActivity extends AppCompatActivity {
             Toast.makeText(this, getString(R.string.library_provider_insert_book_good),
                     Toast.LENGTH_SHORT).show();
         }
-        /*
-        if (mCurrentLibraryUri == null) {
-            // This is a NEW pet, so insert a new pet into the  provider,
-            // returning the content URI for the new pet.
-            newUri = getContentResolver().insert(LibraryContract.LibraryEntry.CONTENT_URI, values);
-
-        }*/
     }
 
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // define a projection that contains all columns in TBooks
+        String[] projection = {LibraryContract.LibraryEntry._ID,
+                LibraryContract.LibraryEntry.COL_TITLE,
+                LibraryContract.LibraryEntry.COL_AUTHOR,
+                LibraryContract.LibraryEntry.COL_YEAR_PUBLISHED,
+                LibraryContract.LibraryEntry.COL_BORROWER};
+
+        // Start a new thread
+        return new CursorLoader(this, mCurrentBookUri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
+        // move to the only row in the Cursor
+        if (c.moveToFirst()){
+            // get the index of each column
+            int titleColumnIndex=c.getColumnIndex(LibraryContract.LibraryEntry.COL_TITLE);
+            int authorColumnIndex=c.getColumnIndex(LibraryContract.LibraryEntry.COL_AUTHOR);
+            int publishYearColumnIndex=c.getColumnIndex(LibraryContract.LibraryEntry.COL_YEAR_PUBLISHED);
+            int borrowerColumnIndex=c.getColumnIndex(LibraryContract.LibraryEntry.COL_BORROWER);
+
+            // use the index to pull the data out
+            String title=c.getString(titleColumnIndex);
+            String author=c.getString(authorColumnIndex);
+            int publishYear = c.getInt(publishYearColumnIndex);
+            String borrower = c.getString(borrowerColumnIndex);
+
+            //Update the text views
+            mTitleEditText.setText(title);
+            mAuthorEditText.setText(author);
+            mBorrowerEditText.setText(borrower);
+            // Get the position of the publish date in the spinner
+            mPublishYearSpinner.setSelection(publishYear - START_YEAR);
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // If invalid Loader clear data from input field
+        mTitleEditText.setText("");
+        mAuthorEditText.setText("");
+        mPublishYearSpinner.setSelection(0);
+        mBorrowerEditText.setText("");
+    }
 }
 
 
