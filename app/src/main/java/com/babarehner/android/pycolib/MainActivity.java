af89f17475037a6a7b926e5d560a346e523b5d64
Mike Rehner <babarehner@google.com>
2016-12-20 16:45:30 -0500

    Add MainActivity.java and activity_main.xml to project. These provide an entry page and allow users to navigate between books, borrowers and check in/checkout

---
+++ app/src/main/java/com/babarehner/android/pycolib/MainActivity.java
@@ -0,0 +1,36 @@
+package com.babarehner.android.pycolib;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        TextView library = (TextView) findViewById(R.id.library_page);
+        library.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent libraryIntent = new Intent(MainActivity.this, LibraryActivity.class);
+                startActivity(libraryIntent);
+            }
+        });
+
+        TextView patron = (TextView) findViewById(R.id.patron_page);
+        patron.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent patronIntent = new Intent(MainActivity.this, LibraryActivity.class);
+                startActivity(patronIntent);
+            }
+        });
+    }
+}
