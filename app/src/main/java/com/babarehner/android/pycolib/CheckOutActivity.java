a67ac60605cefa7b5d25cd9590974383b36365b3
Mike Rehner <babarehner@google.com>
2017-01-27 10:57:09 -0500

    implement DatePicker UI

--- app/src/main/java/com/babarehner/android/pycolib/CheckOutActivity.java
+++ app/src/main/java/com/babarehner/android/pycolib/CheckOutActivity.java
@@ -1,17 +1,35 @@
 package com.babarehner.android.pycolib;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.babarehner.android.pycolib.data.LibraryDbHelper;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class CheckOutActivity extends AppCompatActivity {
 
     Spinner s, b;
+
+    private TextView tvDisplayDate;
+    // Shows calendar when datepicker in activity_check_out_xml
+    // private DatePicker dpResult;
+    private Button btnCheckOutDate;
+
+    private int year;
+    private int month;
+    private int day;
+
+    static final int DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +56,66 @@ public class CheckOutActivity extends AppCompatActivity {
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         b.setAdapter(dataAdapter2);
 
+        addListenerButton();
+        setCurrentDateOnView();
+
     }
+
+    public void setCurrentDateOnView() {
+        tvDisplayDate = (TextView) findViewById(R.id.tvDate);
+        // dpResult = (DatePicker) findViewById(R.id.dpResult);
+
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        tvDisplayDate.setText(new StringBuilder()
+            // Month starts with 0, add 1)
+            .append(month + 1).append("-").append(day).append("-")
+            .append(year).append(" "));
+
+        // set current date into datepicker
+        // dpResult.init(year, month, day, null);
+    }
+
+    public void addListenerButton() {
+        btnCheckOutDate = (Button) findViewById(R.id.button_borrow);
+        btnCheckOutDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                // set date picker as current date
+                return new DatePickerDialog(this, datePickerListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener =
+            new DatePickerDialog.OnDateSetListener(){
+
+        // when dialog box is closed, below method will be called
+        public void onDateSet(DatePicker v, int selectedYear, int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+
+            // set selected date into textview
+            tvDisplayDate.setText(new StringBuilder().append(month + 1)
+                    .append("-").append(day).append("-").append(year)
+                    .append(" "));
+
+            // dpResult.init(year, month, day, null);
+        }
+    };
     /*
     } public DbBackend(Context context) {
         super(context);
